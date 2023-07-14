713f0a29e4029384420433bc4102f63024ac9b0c
Willian Sato <willianmassayukisato@gmail.com>
2023-07-14 14:37:17 -0300

    Responsividade mobile

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,27 +14,32 @@ const Comment = ({objeto}) => {
   }
 
   function deleteComment(id) {
-    // Obter os dados armazenados no localStorage
     const storedData = localStorage.getItem('dados');
   
     if (storedData) {
-      // Converter os dados de volta para um objeto
       const parsedData = JSON.parse(storedData);
   
-      // Encontrar o índice do elemento com o ID fornecido
-      const index = parsedData.findIndex(item => item.id === id);
-      console.log(index);
+      // Filtrar os comentários, removendo o item com o ID correspondente
+      const updatedComments = parsedData.comments.filter(comment => comment.id !== id);
   
-      if (index !== -1) {
-        // Remover o elemento do array
-        parsedData.splice(index, 1);
+      // Filtrar as respostas, removendo os itens com o ID correspondente
+      const updatedReplies = parsedData.comments.map(comment => {
+        const updatedComment = { ...comment };
+        updatedComment.replies = updatedComment.replies.filter(reply => reply.id !== id);
+        return updatedComment;
+      });
   
-        // Atualizar o localStorage com os dados atualizados
-        localStorage.setItem('dados', JSON.stringify(parsedData));
+      // Atualizar o objeto com os comentários e respostas atualizados
+      parsedData.comments = updatedComments;
+      parsedData.comments.forEach((comment, index) => {
+        comment.replies = updatedReplies[index].replies;
+      });
   
-        // Atualizar o estado do componente, se necessário
-        setDados(parsedData);
-      }
+      // Atualizar o localStorage com os dados atualizados
+      localStorage.setItem('dados', JSON.stringify(parsedData));
+  
+      // Atualizar o estado do componente, se necessário
+      setNovoObjeto(parsedData);
     }
   }
 
@@ -61,7 +66,7 @@ const Comment = ({objeto}) => {
                 <button className='comment__reply-button delete-button' onClick={() => deleteComment(data.id)}><img src="/images/icon-delete.svg" alt="Delete"/>Delete</button>
                 <button className='comment__reply-button' onClick={() => editComment(data.id)}><img src="/images/icon-edit.svg" alt="Edit"/>Edit</button>
               </div>
-            :
+              :
               <div className='author-buttons'>
                 <button className='comment__reply-button' onClick={() => show(data.id)}><img src="/images/icon-reply.svg" alt="Reply"/>Reply</button>
               </div>
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -21,27 +21,32 @@ const Reply = ({objeto, replyData}) => {
   }
 
   function deleteComment(id) {
-    // Obter os dados armazenados no localStorage
     const storedData = localStorage.getItem('dados');
   
     if (storedData) {
-      // Converter os dados de volta para um objeto
       const parsedData = JSON.parse(storedData);
   
-      // Encontrar o índice do elemento com o ID fornecido
-      const index = parsedData.findIndex(item => item.comments.map((dados) => (dados.id === id) ));
-      console.log(index);
+      // Filtrar os comentários, removendo o item com o ID correspondente
+      const updatedComments = parsedData.comments.filter(comment => comment.id !== id);
   
-      if (index !== -1) {
-        // Remover o elemento do array
-        parsedData.splice(index, 1);
+      // Filtrar as respostas, removendo os itens com o ID correspondente
+      const updatedReplies = parsedData.comments.map(comment => {
+        const updatedComment = { ...comment };
+        updatedComment.replies = updatedComment.replies.filter(reply => reply.id !== id);
+        return updatedComment;
+      });
   
-        // Atualizar o localStorage com os dados atualizados
-        localStorage.setItem('dados', JSON.stringify(parsedData));
+      // Atualizar o objeto com os comentários e respostas atualizados
+      parsedData.comments = updatedComments;
+      parsedData.comments.forEach((comment, index) => {
+        comment.replies = updatedReplies[index].replies;
+      });
   
-        // Atualizar o estado do componente, se necessário
-        setDados(parsedData);
-      }
+      // Atualizar o localStorage com os dados atualizados
+      localStorage.setItem('dados', JSON.stringify(parsedData));
+  
+      // Atualizar o estado do componente, se necessário
+      setDados(parsedData);
     }
   }
 
